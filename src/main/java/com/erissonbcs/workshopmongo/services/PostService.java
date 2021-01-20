b87796a608f0fcc2d292a4addb6ec33e00ed6137
Erisson Batista <erissonbcs@gmail.com>
2021-01-20 12:05:35 -0300

    Obtendo um post por id

---
+++ src/main/java/com/erissonbcs/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.erissonbcs.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.erissonbcs.workshopmongo.domain.Post;
+import com.erissonbcs.workshopmongo.repository.PostRepository;
+import com.erissonbcs.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
